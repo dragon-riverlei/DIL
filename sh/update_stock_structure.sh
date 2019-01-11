6e6a6a30e62d9c279bbc790d72152963bfcdd25f
hel <com.hhh.hel@gmail.com>
2019-01-11 09:30:04 +0800

    Change the sql statement to use function check_securities_kpis_3_cap().

--- sh/update_stock_structure.sh
+++ sh/update_stock_structure.sh
@@ -5,7 +5,7 @@
 # Populate "DIL_ROOT" with proper value
 . $(dirname "$0")/env.sh
 
-codes=$(psql dil -t -c "select case when sc.market = 'Shanghai' then 'sh' || t2.code else 'sz' || t2.code end code from (select code, 市值 - cap diff_cap from (select tk.code, tk.time, round(tk.市值,-6) 市值, dq.cap from (select * from securities_kpis_3()) tk join securities_day_quote dq on tk.code = dq.code and tk.time = dq.time and dq.code in (select distinct code from securities_profit_sheet_running_total) order by code) t1) t2 join securities_code sc on t2.code = sc.code where t2.diff_cap <> 0 and t2.code in (select distinct code from securities_profit_sheet_running_total);" | awk '{print $1}' | tr '\n' ',')
+codes=$(psql dil -t -c "select case when sc.market = 'Shanghai' then 'sh' || kpi3.code else 'sz' || kpi3.code end from (select code from check_securities_kpis_3_cap()) kpi3 join securities_code sc on sc.code = kpi3.code;" | awk '{print $1}' | tr '\n' ',')
 
 cd "$DIL_ROOT/crawl/crawl"
 echo "scraping stock structure from web..."
