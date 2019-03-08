4a66c2a7eaf242d3b6c5567c488b5cf63df54368
hel <com.hhh.hel@gmail.com>
2019-03-08 10:58:53 +0800

    Fix bug: naming mismatch.

--- sh/update_stock_structure.sh
+++ sh/update_stock_structure.sh
@@ -5,7 +5,7 @@
 # Populate "DIL_ROOT" with proper value
 . $(dirname "$0")/env.sh
 
-codes=$(psql dil -t -c "select case when sc.market = 'Shanghai' then 'sh' || kpi3.code else 'sz' || kpi3.code end from (select code from check_securities_kpis_3_cap()) kpi3 join securities_code sc on sc.code = kpi3.code;" | awk '{print $1}' | tr '\n' ',')
+codes=$(psql dil -t -c "select case when sc.market = 'Shanghai' then 'sh' || kpi3.code else 'sz' || kpi3.code end from (select code from check_securities_kpi_c3_cap()) kpi3 join securities_code sc on sc.code = kpi3.code;" | awk '{print $1}' | tr '\n' ',')
 
 cd "$DIL_ROOT/crawl/crawl"
 echo "scraping stock structure from web..."
