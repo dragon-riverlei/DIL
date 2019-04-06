9d9754bbbb3797f5d36b6dc2590b1405232606c5
hel <com.hhh.hel@gmail.com>
2019-04-06 12:12:11 +0800

    Fix bug: bash scripts to update regular reports on given year and month don't work when parameter "codes" is not given.
    closes #31.

--- sh/update_balance_sheet.sh
+++ sh/update_balance_sheet.sh
@@ -23,96 +23,208 @@ fi
 
 spider="StockFdmtBalanceSheetChinaDeltaSpider"
 
-
 # Populate "DIL_ROOT" with proper value
 . $(dirname "$0")/env.sh
 
+load_data_to_db(){
+    local code="$1"
+    local jqf="$2"
+    local jl_file="$3"
+    local csv_file="$4"
+    local table="$5"
+    local year="$6"
+    local month="$7"
+    jq -r --arg code "$code" -f "$jqf" "$jl_file" > "$csv_file"
+    psql "$db" <<EOF
+create temp table tmp_table as select * from $table with no data;
+copy tmp_table from '$csv_file' with (format csv);
+delete from $table where code = '$code' and extract(year from time) = $year;
+insert into $table select * from tmp_table on conflict do nothing;
+EOF
+}
+
+echo "Updating balance sheet..."
 cd "$DIL_ROOT"/crawl/crawl
 if [ "$3" == "" ];then
+    echo "Scraping balance sheet data..."
     scrapy crawl "$spider" -a year="$1" -a month="$2"
+    echo "Scraping balance sheet data done."
+
+    echo "Checking and loading balance sheet data to DB..."
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000001.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_bank.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs bank > "$jqf"
+    table=securities_balance_sheet_bank
+    for code in $(cat "$schema_lst" <("$DIL_ROOT"/sh/find_regular_report_not_scraped_delta.sh bs "$1" "$2") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_bank.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000002.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_general.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs general > "$jqf"
+    table=securities_balance_sheet_general
+    for code in $(cat "$schema_lst" <("$DIL_ROOT"/sh/find_regular_report_not_scraped_delta.sh bs "$1" "$2") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_general.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/601318.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_insurance.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs insurance > "$jqf"
+    table=securities_balance_sheet_insurance
+    for code in $(cat "$schema_lst" <("$DIL_ROOT"/sh/find_regular_report_not_scraped_delta.sh bs "$1" "$2") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_insurance.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000686.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_securities.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs securities > "$jqf"
+    table=securities_balance_sheet_securities
+    for code in $(cat "$schema_lst" <("$DIL_ROOT"/sh/find_regular_report_not_scraped_delta.sh bs "$1" "$2") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_securities.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+    echo "Checking and loading balance sheet data to DB done."
 else
+    echo "Scraping balance sheet data..."
     scrapy crawl "$spider" -a year="$1" -a month="$2" -a codes="$3"
+    echo "Scraping balance sheet data done."
+
+    echo "Checking and loading balance sheet data to DB..."
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000001.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_bank.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs bank > "$jqf"
+    table=securities_balance_sheet_bank
+    for code in $(cat "$schema_lst_bank" <(echo "$3" | tr "," "\n") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_bank.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000002.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_general.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs general > "$jqf"
+    table=securities_balance_sheet_general
+    for code in $(cat "$schema_lst_general" <(echo "$3" | tr "," "\n") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_general.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/601318.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_insurance.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs insurance > "$jqf"
+    table=securities_balance_sheet_insurance
+    for code in $(cat "$schema_lst_insurance" <(echo "$3" | tr "," "\n") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_insurance.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+
+    schema_lst="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000686.lst
+    jqf="$DIL_ROOT"/tmp/extract_balance_sheet_securities.jq
+    "$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs securities > "$jqf"
+    table=securities_balance_sheet_securities
+    for code in $(cat "$schema_lst_securities" <(echo "$3" | tr "," "\n") | sort | uniq -d)
+    do
+        jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
+        if [ "$2" -eq 3 ] || [ "$2" -eq 12 ];then
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-31)
+        else
+            count=$(cat "$jl_file" | grep -c "$1"-"$2"-30)
+        fi
+        if [ "$count" -eq 1 ];then
+            csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_"$1".csv
+            check_result=$("$DIL_ROOT"/sh/check_balance_sheet_securities.sh "$jl_file")
+            if [ "$check_result" == "" ];then
+                load_data_to_db "$code" "$jqf" "$jl_file" "$csv_file" "$table" "$1" "$2"
+            fi
+        fi
+    done
+    echo "Checking and loading balance sheet data to DB done."
 fi
-
-schema_lst_bank="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000001.lst
-jqf_bank="$DIL_ROOT"/tmp/extract_balance_sheet_bank.jq
-"$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs bank > "$jqf_bank"
-table=securities_balance_sheet_bank
-for code in $(cat "$schema_lst_bank" <(echo "$3" | tr "," "\n") | sort | uniq -d)
-do
-    jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_$1.csv
-    check_result=$("$DIL_ROOT"/sh/check_balance_sheet_bank.sh "$jl_file")
-    if [ "$check_result" == "" ];then
-        jq -r --arg code "$code" -f "$jqf_bank" "$jl_file" > "$csv_file"
-        psql "$db" <<EOF
-create temp table tmp_table as select * from $table with no data;
-copy tmp_table from '$csv_file' with (format csv);
-delete from $table where code = '$code' and extract(year from time) = $1;
-insert into $table select * from tmp_table on conflict do nothing;
-EOF
-    fi
-done
-
-schema_lst_general="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000002.lst
-jqf_general="$DIL_ROOT"/tmp/extract_balance_sheet_general.jq
-"$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs general > "$jqf_general"
-table=securities_balance_sheet_general
-for code in $(cat "$schema_lst_general" <(echo "$3" | tr "," "\n") | sort | uniq -d)
-do
-    "$DIL_ROOT"/sh/check_balance_sheet_general.sh "$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_$1.csv
-    check_result=$("$DIL_ROOT"/sh/check_balance_sheet_general.sh "$jl_file")
-    if [ "$check_result" == "" ];then
-        jq -r --arg code "$code" -f "$jqf_general" "$jl_file" > "$csv_file"
-        psql "$db" <<EOF
-create temp table tmp_table as select * from $table with no data;
-copy tmp_table from '$csv_file' with (format csv);
-delete from $table where code = '$code' and extract(year from time) = $1;
-insert into $table select * from tmp_table on conflict do nothing;
-EOF
-    fi
-done
-
-schema_lst_insurance="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/601318.lst
-jqf_insurance="$DIL_ROOT"/tmp/extract_balance_sheet_insurance.jq
-"$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs insurance > "$jqf_insurance"
-table=securities_balance_sheet_insurance
-for code in $(cat "$schema_lst_insurance" <(echo "$3" | tr "," "\n") | sort | uniq -d)
-do
-    "$DIL_ROOT"/sh/check_balance_sheet_insurance.sh "$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_$1.csv
-    check_result=$("$DIL_ROOT"/sh/check_balance_sheet_insurance.sh "$jl_file")
-    if [ "$check_result" == "" ];then
-        jq -r --arg code "$code" -f "$jqf_insurance" "$jl_file" > "$csv_file"
-        psql "$db" <<EOF
-create temp table tmp_table as select * from $table with no data;
-copy tmp_table from '$csv_file' with (format csv);
-delete from $table where code = '$code' and extract(year from time) = $1;
-insert into $table select * from tmp_table on conflict do nothing;
-EOF
-    fi
-done
-
-schema_lst_securities="$DIL_ROOT"/feeds/fdmt_balance_sheet_initial/schemas/000686.lst
-jqf_securities="$DIL_ROOT"/tmp/extract_balance_sheet_securities.jq
-"$DIL_ROOT"/sh/print_regular_report_delta_jq_fields.sh -e bs securities > "$jqf_securities"
-table=securities_balance_sheet_securities
-for code in $(cat "$schema_lst_securities" <(echo "$3" | tr "," "\n") | sort | uniq -d)
-do
-    "$DIL_ROOT"/sh/check_balance_sheet_securities.sh "$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    jl_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/data/"$code"_"$1".jl
-    csv_file="$DIL_ROOT"/feeds/fdmt_balance_sheet_delta/dbdata/"$code"_$1.csv
-    check_result=$("$DIL_ROOT"/sh/check_balance_sheet_securities.sh "$jl_file")
-    if [ "$check_result" == "" ];then
-        jq -r --arg code "$code" -f "$jqf_securities" "$jl_file" > "$csv_file"
-        psql "$db" <<EOF
-create temp table tmp_table as select * from $table with no data;
-copy tmp_table from '$csv_file' with (format csv);
-delete from $table where code = '$code' and extract(year from time) = $1;
-insert into $table select * from tmp_table on conflict do nothing;
-EOF
-    fi
-done
