1a8f56ee1d9a01e4aa6743c40bcbeb55061c3bb0
hel <com.hhh.hel@gmail.com>
2019-04-06 12:15:49 +0800

    Implement feature: bash script to update balance, cash flow and profit sheet in one go.
    closes #32.

---
+++ sh/update_balance_cash_flow_profit_sheets.sh
@@ -0,0 +1,47 @@
+#!/usr/local/bin/bash
+
+# Update balance sheet, cash flow sheet and profit sheet.
+
+usage(){
+    echo "usage:"
+    echo "update_balance_cash_flow_profit_sheets.sh <year> <month>"
+    echo "update_balance_cash_flow_profit_sheets.sh <year> <month> <codes>"
+    exit 1
+}
+
+if [ "$(date -j -f '%Y-%m-%d' $1-12-31)" == "" ];then
+    usage
+fi
+
+if [ ! -n "$2" ] || [ ! "$2" -eq "$2" ]; then
+    usage
+fi
+
+if [ ! "$2" -eq 3 ] && [ ! "$2" -eq 6 ] && [ ! "$2" -eq 9 ] && [ ! "$2" -eq 12 ]; then
+    usage
+fi
+
+. $(dirname "$0")/update_balance_sheet.sh "$1" "$2" "$3"
+. $(dirname "$0")/update_cash_flow_sheet.sh "$1" "$2" "$3"
+. $(dirname "$0")/update_profit_sheet.sh "$1" "$2" "$3"
+
+. $(dirname "$0")/env.sh
+
+echo "Updating table securities_cash_flow_sheet_running_total..."
+psql "$db" <<EOF
+select * from insert_securities_cash_flow_sheet_running_total_single_year($1);
+EOF
+echo "Updating table securities_cash_flow_sheet_running_total done."
+
+echo "Updating table securities_profit_sheet_running_total..."
+psql "$db" <<EOF
+select * from insert_securities_profit_sheet_running_total_single_year($1);
+EOF
+echo "Updating table securities_profit_sheet_running_total done."
+
+echo "Updating table securities_kpi..."
+psql "$db" <<EOF
+select * from insert_securities_kpi_c1($1,$1);
+select * from insert_securities_kpi_c2($1,$1);
+EOF
+echo "Updating table securities_kpi done."
